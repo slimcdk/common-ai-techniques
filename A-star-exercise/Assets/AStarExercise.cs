f7feea80f612e3118f1415044116c65cebe7218f
Christian Skjerning <christian@skjerning.eu>
2019-03-01 16:01:23 +0100

    empty a-star

---
+++ A-star-exercise/Assets/AStarExercise.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AStarExercise : MonoBehaviour {
+
+    public Node start;
+    public Node goal;
+
+	// Use this for initialization
+	void Start () {
+
+        List<Node> closedSet = new List<Node>();
+
+        List<Node> openSet = new List<Node>();
+
+        //initially we set the openSet as our starting node
+        openSet.Add(start);
+
+        //cost to go to start is 0 so the cost is just the heuristic
+        start.cost = 0 + HeuristicCostToGoal(start);
+
+        while (openSet.Count > 0)
+        {
+            //IMPLEMENT A* HERE
+        }
+	}
+
+    void ReconstructPath(Node current)
+    {
+        if (current == start)
+            return;
+        Node bestNode = null;
+        float minCost = float.PositiveInfinity;
+        foreach (Node n in current.neighbors)
+        {
+            if (n.cost < minCost)
+            {
+                bestNode = n;
+                minCost = n.cost;
+            }
+        }
+        Debug.Log("coloring edge between " + current + " and " + bestNode);
+        current.edges[bestNode].GetComponent<LineRenderer>().startColor=Color.red;
+        current.edges[bestNode].GetComponent<LineRenderer>().endColor = Color.red;
+        ReconstructPath(bestNode);
+    }
+
+    float CostToGetToTheNode(Node from, Node to)
+    {
+        return from.cost + Vector3.Distance(from.gameObject.transform.position, to.gameObject.transform.position);
+    }
+
+    float HeuristicCostToGoal(Node n)
+    {
+        return Vector3.Distance(n.gameObject.transform.position, goal.gameObject.transform.position);
+    }
+	
+}
---
+++ A-star-exercise/Assets/Node.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    public List<Node> neighbors;
+    public Dictionary<Node, GameObject> edges = new Dictionary<Node, GameObject>();
+    public GameObject edgePrefab;
+
+    public float cost = float.PositiveInfinity;
+
+    // Use this for initialization
+    void Awake () {
+        foreach (Node n in neighbors)
+        {
+            //create graphical edge
+            if (!edges.ContainsKey(n))
+            {
+                GameObject edge = Instantiate(edgePrefab);
+                LineRenderer lineRenderer = edge.AddComponent<LineRenderer>();
+                lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+                lineRenderer.widthMultiplier = 0.2f;
+                lineRenderer.SetPositions(new Vector3[] { transform.position, n.transform.position });
+                edges.Add(n, edge);
+                n.edges.Add(this, edge);
+            }
+            
+
+            //make sure the neighbor has the connection as well (meaning we have no one-way edges)
+            if (!n.neighbors.Contains(this))
+            {
+                n.neighbors.Add(this);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
